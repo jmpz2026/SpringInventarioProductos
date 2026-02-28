fd59757da67719738b3218cded7fdc61d855c18d
elcorremachitos <elcorremachitos.2015@gmail.com>
2026-02-27 22:33:33 -0500

    First Version

---
+++ src/main/java/com/springinventarioproductos/SpringInventarioProductosApplication.java
@@ -0,0 +1,13 @@
+package com.springinventarioproductos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInventarioProductosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInventarioProductosApplication.class, args);
+    }
+
+}
